eaf5d53092509bd5cc96bd5ff5529c1666ce5042
jerrycaffe <jerrycaffe@gmail.com>
2024-12-29 23:47:40 +0100

    saved working on main

--- app/src/main/java/com/example/trustline/MainActivity.kt
+++ app/src/main/java/com/example/trustline/MainActivity.kt
@@ -4,11 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,7 +19,7 @@ class MainActivity : ComponentActivity() {
             TrustlineTheme {
                 Scaffold(modifier = Modifier.systemBarsPadding()) { innerPadding ->
 
-                    SplashScreen(Modifier.padding(innerPadding))
+                    SplashScreen()
                 }
             }
         }
@@ -30,11 +27,10 @@ class MainActivity : ComponentActivity() {
 }
 
 
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TrustlineTheme {
-        WelcomeView()
+        SplashScreen()
     }
 }
